e3d5c431d6540ab2eaf9ef3932723dd5b865a85f
ozanmirza1 <ozan.mirza@icloud.com>
2018-05-15 10:45:06 -0400

    Ozan Is Awesome

--- game.py
+++ game.py
@@ -2,23 +2,17 @@ import pygame
 from pygame.locals import *
 import sys # Imports the System Module
 import random
+
 class GameSprite(pygame.sprite.Sprite):
-  """ This class represents a sprite. It derives from the "Sprite"
-      class in Pygame.
-  """
 
   def __init__(self, screen, filename, position):
-    """ Constructor. Pass in the screen, the name of image file to open,
-        and its starting position (x, y). 
-    """
     pygame.sprite.Sprite.__init__(self) # call the parent (Sprite) constructor
     self.screen = screen
     self.image = pygame.image.load(filename)
     self.position = position
     self.rect = self.image.get_rect() # the image's rectangle
-   
+
   def update(self, dx, dy):
-    """ Update the sprite's position """
     x, y = self.position
     x += dx
     y += dy
@@ -27,6 +21,24 @@ class GameSprite(pygame.sprite.Sprite):
     self.rect.center = self.position # set new position of sprite
 
   def draw(self):
-    """ Draw the sprite on the screen """
     self.screen.blit(self.image, self.rect)
-          
+
+class connectFour():
+    def __init__(self, width, height):
+        pygame.init()
+        self.screen_width = width
+        self.screen_height = height
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Connect4 Game By Mikey Jacobs And Ozan Mirza")
+
+    def run(self):
+        clock = pygame.time.Clock()
+
+
+while True:
+    play = raw_input("Would you like to play Connect4 by Mikey Jacobs and Ozan Mirza? (y/n): ")
+    if play == "y":
+        game = connectFour(1000, 500)
+        game.run()
+    elif play == "n":
+        sys.exit(0)
