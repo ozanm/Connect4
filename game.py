4be5f2427c050ebde89e7a2da8d7299aa39b215d
ozanmirza1 <ozan.mirza@icloud.com>
2018-05-15 10:28:57 -0400

    Ozan Is Awesome

--- game.py
+++ game.py
@@ -1,11 +1,3 @@
 import pygame
 from pygame.locals import *
 import sys # Imports the System Module
-window_width = 1000
-window_height = 650
-window_size = window_width, window_height
-screen = pygame.display.set_mode(window_size)
-red_circle = None
-black_circle = None
-screen_color = (0, 0, 255)
-screen.fill(screen_color)
