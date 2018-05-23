0a59834e7dcc2c648ecb29aa4deadf8491472a10
ozanmirza1 <ozan.mirza@icloud.com>
2018-05-23 08:55:17 -0400

    Ozan Is Awesome

--- game.py
+++ game.py
@@ -32,6 +32,12 @@ class connectFour():
         pygame.display.set_caption("Connect4 Game By Mikey Jacobs And Ozan Mirza")
         self.black = GameSprite(self.screen, '4row_black.png', (150,550))
         self.red = GameSprite(self.screen, '4row_red.png', (750,550))
+        self.blocks = [[None, None, None, None, None, None],
+                       [None, None, None, None, None, None],
+                       [None, None, None, None, None, None],
+                       [None, None, None, None, None, None],
+                       [None, None, None, None, None, None],
+                       [None, None, None, None, None, None]]
 
     def run(self, running):
         pygame.init()
@@ -40,7 +46,9 @@ class connectFour():
         while running == True:
             clock.tick(30)
             for event in pygame.event.get():
-                if not hasattr(event, 'key'): # if its not a key event then ignore it
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif not hasattr(event, 'key'): # if its not a key event then ignore it
                     continue
             print "Connect4 Game Running At " + str(clock.get_fps()) + " FPS"
             self.screen.fill((0, 255, 255))
@@ -48,6 +56,12 @@ class connectFour():
             self.red.update(0, 0)
             self.black.draw()
             self.red.draw()
+
+            for i in range(0, 6):
+                for j in range(0, 6):
+                    self.blocks[i][j] = GameSprite(self.screen, '4row_board.png', (i * 2, j * 2))
+                    self.blocks[i][j].update(0, 0)
+                    self.blocks[i][j].draw()
             pygame.display.flip()
 
 play = raw_input("Would you like to play Connect4 by Mikey Jacobs and Ozan Mirza? (y/n): ")
